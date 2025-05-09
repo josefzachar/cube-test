92d9785c75aef66d2fbb7f6b597c118859624b4a
josefzachar <josefzachar@users.noreply.github.com>
2025-05-09 18:51:36 +0200

    Cells not moving after performance upgrade fixed

--- src/updater.lua
+++ src/updater.lua
@@ -124,6 +124,7 @@ end
 
 -- Update active clusters
 function Updater.updateActiveClusters(level, dt, ball)
+    local Cell = require("cell")
     -- Reset all clusters to inactive
     local clusterRows = math.ceil(level.height / level.clusterSize)
     local clusterCols = math.ceil(level.width / level.clusterSize)
@@ -172,6 +173,84 @@ function Updater.updateActiveClusters(level, dt, ball)
             -- Also mark clusters below as active (for falling sand)
             if clusterY + 1 < clusterRows then
                 level.clusters[clusterY + 1][clusterX].active = true
+            end
+        end
+    end
+    
+    -- NEW: Check for sand and water cells that should be moving
+    -- This ensures cells don't freeze in mid-air when not near the ball
+    for y = 0, level.height - 1 do
+        for x = 0, level.width - 1 do
+            if level.cells[y] and level.cells[y][x] then
+                local cellType = level.cells[y][x].type
+                
+                -- Check if this is a sand or water cell
+                if cellType == Cell.TYPES.SAND or cellType == Cell.TYPES.WATER then
+                    -- Check if there's empty space or water below (for sand)
+                    local shouldActivate = false
+                    
+                    -- Check below
+                    if y < level.height - 1 then
+                        local belowType = level.cells[y+1][x].type
+                        if belowType == Cell.TYPES.EMPTY or 
+                           (cellType == Cell.TYPES.SAND and belowType == Cell.TYPES.WATER) then
+                            shouldActivate = true
+                        end
+                    end
+                    
+                    -- Check diagonally below
+                    if not shouldActivate and y < level.height - 1 then
+                        -- Check left diagonal
+                        if x > 0 then
+                            local diagLeftType = level.cells[y+1][x-1].type
+                            if diagLeftType == Cell.TYPES.EMPTY or 
+                               (cellType == Cell.TYPES.SAND and diagLeftType == Cell.TYPES.WATER) then
+                                shouldActivate = true
+                            end
+                        end
+                        
+                        -- Check right diagonal
+                        if x < level.width - 1 then
+                            local diagRightType = level.cells[y+1][x+1].type
+                            if diagRightType == Cell.TYPES.EMPTY or 
+                               (cellType == Cell.TYPES.SAND and diagRightType == Cell.TYPES.WATER) then
+                                shouldActivate = true
+                            end
+                        end
+                    end
+                    
+                    -- For water, also check horizontally
+                    if not shouldActivate and cellType == Cell.TYPES.WATER then
+                        -- Check left
+                        if x > 0 and level.cells[y][x-1].type == Cell.TYPES.EMPTY then
+                            shouldActivate = true
+                        end
+                        
+                        -- Check right
+                        if x < level.width - 1 and level.cells[y][x+1].type == Cell.TYPES.EMPTY then
+                            shouldActivate = true
+                        end
+                    end
+                    
+                    -- If this cell should be moving, activate its cluster
+                    if shouldActivate then
+                        local clusterX = math.floor(x / level.clusterSize)
+                        local clusterY = math.floor(y / level.clusterSize)
+                        
+                        if clusterY >= 0 and clusterY < clusterRows and 
+                           clusterX >= 0 and clusterX < clusterCols then
+                            level.clusters[clusterY][clusterX].active = true
+                            
+                            -- Also mark the cluster below as active
+                            if clusterY + 1 < clusterRows then
+                                level.clusters[clusterY + 1][clusterX].active = true
+                            end
+                            
+                            -- Add this cell to active cells for next frame
+                            table.insert(level.activeCells, {x = x, y = y})
+                        end
+                    end
+                end
             end
         end
     end
